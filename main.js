c82c2ee5d626e7fb09e2c5c71070dd9ea0bf902d
JP <parker.tech9@gmail.com>
2019-03-30 10:49:51 -0400

    bootstrap layout

---
+++ main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCO0SVX11y1TJ7TLZoCJhMEgKnLTE3MZYw",
+  authDomain: "data-management-rut.firebaseapp.com",
+  databaseURL: "https://data-management-rut.firebaseio.com",
+  projectId: "data-management-rut",
+  storageBucket: "data-management-rut.appspot.com",
+  messagingSenderId: "825932167890"
+};
+firebase.initializeApp(config);
